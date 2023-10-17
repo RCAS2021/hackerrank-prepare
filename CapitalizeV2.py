b78a988b9bd68d3b54442f7a0bc84f00fc00c9c2
Rafael Cruz <rcas2009@outlook.com>
2023-10-17 20:22:13 -0300

    Capitalize

--- CapitalizeV2.py
+++ CapitalizeV2.py
@@ -1,22 +1,10 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the solve function below.
 def solve(s):
-    arr = s.split()
-    c = []
-    for i in range(len(arr)):
-       c.append(arr[i].capitalize())
-    return c
+    words = s.split()
+    capitalized_words = []
+    for i in range(len(words)):
+       capitalized_words.append(words[i].capitalize())
+    return capitalized_words
 
 if __name__ == '__main__':
-    s = input()
-
-    text = solve(s)
-
+    text = solve(input())
     print(*text)
