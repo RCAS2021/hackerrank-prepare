3da8cf646c3e37d0c0dede2f7d88a4faff4d20d9
Rafael Cruz <rcas2009@outlook.com>
2023-10-18 20:46:22 -0300

    Update Time Conversion

--- InterviewKitDay1/Time Conversion.py
+++ InterviewKitDay1/Time Conversion.py
@@ -1,20 +1,22 @@
 def timeConversion(s):
     format = s.split(":")
     if("PM" in format[2]):
+        format[2] = format[2].strip("PM")
         hour = int(format[0])
         if(hour == 12):
             hour = 12
         else:
             hour += 12
-            format[0] = hour
-        format[2] = format[2].strip("PM")
-        print(f"{format[0]}:{format[1]}:{format[2]}")
+            format[0] = str(hour)
+        format = ":".join(format)
+        print(format)
     else:
         format[2] = format[2].strip("AM")
         hour = int(format[0])
         if(hour == 12):
             format[0] = "00"
-        print(f"{format[0]}:{format[1]}:{format[2]}")
+        format = ":".join(format)
+        print(format)
     
 if __name__ == '__main__':
     s = input()
