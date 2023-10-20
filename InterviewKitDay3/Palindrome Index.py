20c032470e2766a6e759b43d33f9a42767276f11
Rafael Cruz <rcas2009@outlook.com>
2023-10-20 12:42:04 -0300

    Update Palindrome Index

--- InterviewKitDay3/Palindrome Index.py
+++ InterviewKitDay3/Palindrome Index.py
@@ -1,12 +1,8 @@
 def palindromeIndex(s):
-    tw = []
     teste = []
-    no = []
+
     for i in range(len(s)):
-        tw.append(s[i])
-    
-    for i in range(len(tw)):
-        if(tw[i] == tw[len(tw)-i-1]):
+        if(s[i] == s[len(s)-i-1]):
             teste.append(True)
         else:
             if s[i+1] == s[-i-1]:
@@ -14,11 +10,7 @@ def palindromeIndex(s):
             elif s[-i-2] == s[i]:
                 return  len(s)-i-1
     if(all(teste) == True):
-        return -1
-    else:
-        return no[0]
-        
-            
+        return -1         
 
 if __name__ == '__main__':
 
